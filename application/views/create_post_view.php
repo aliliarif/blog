368cb24daf0696406b0ae09631581770c57e2c34
fnklifi <fnklifi@gmial.com>
2015-12-13 19:21:30 +0100

    changed top marging

--- application/views/create_post_view.php
+++ application/views/create_post_view.php
@@ -1,16 +1,17 @@
-<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1" style="margin-bottom:30px;">
-    <button class="backBtn btnCustom btnCustom-default">&larr; Back</button>
-    <button id="publish_post" name="publish_post" class="btnCustom pull-right">Publish</button>
-</div>
-
-<div class="row">
-    <form class="form-inline">
-        <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
-            <label>Title: </label>
-            <input type="text" id="post_title" name="post_title" class="form-control" style="margin-bottom:15px; width:68%;" autofocus>
-            <textarea id="post_desc" name="post_desc" class="textarea" placeholder="Description"></textarea>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1" style="margin-bottom:30px; margin-top:40px;">
+            <button class="backBtn btnCustom btnCustom-default">&larr; Back</button>
+            <button id="publish_post" name="publish_post" class="btnCustom pull-right">Publish</button>
         </div>
-    </form>
+        <form class="form-inline">
+            <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
+                <label>Title: </label>
+                <input type="text" id="post_title" name="post_title" class="form-control" style="margin-bottom:15px; width:68%;" autofocus>
+                <textarea id="post_desc" name="post_desc" class="textarea" placeholder="Description"></textarea>
+            </div>
+        </form>
+    </div>
 </div>
     
 
